87edc718481208cf8e1fada17f4e68fec5aa135b
Satoru Hayasaka <hayasaka@utexas.edu>
2018-06-13 06:45:13 -0500

    Scalar function example (Listing 4-1)

---
+++ Theano_FuncScalar.py
@@ -0,0 +1,17 @@
+import theano.tensor as T
+from theano import function
+
+a = T.dscalar('a')
+b = T.dscalar('b')
+c = T.dscalar('c')
+d = T.dscalar('d')
+e = T.dscalar('e')
+
+f = ((a - b + c) * d )/e
+
+g = function([a, b, c, d, e], f)
+
+print("Expected: ((1 - 2 + 3) * 4)/5.0 = ", ((1 - 2 + 3) * 4)/5.0)
+print("Via Theano: ((1 - 2 + 3) * 4)/5.0 = ",  g(1, 2, 3, 4, 5))
+# Expected: ((1 - 2 + 3) * 4)/5.0 =  1.6
+# Via Theano: ((1 - 2 + 3) * 4)/5.0 =  1.6
