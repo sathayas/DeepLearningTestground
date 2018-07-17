9d2f312e249aa00afb2cf7321fd709b2cca51982
Satoru Hayasaka <hayasaka@utexas.edu>
2018-07-17 12:44:51 -0500

    Linear regression, completed as described in Listing 4-11

--- Theano/Theano_LinReg.py
+++ Theano/Theano_LinReg.py
@@ -16,7 +16,45 @@ def squared_error(x,y):
 examples = 1000
 features = 100
 training_steps = 1000
+alpha = 0.1
+lamb = 0.01  # regularization parameter for the loss function
 
 # generating random data
-D = (numpy.random.randn(examples, features), 
-     numpy.random.randn(examples))
+D = (np.random.randn(examples, features), 
+     np.random.randn(examples))
+
+# defining variables and such for Theano
+x = T.dmatrix("x")
+y = T.dvector("y")
+# weights, initialized with random numbers
+w = theano.shared(np.random.randn(features), name="w")
+# bias, starts out with 0
+b = theano.shared(0., name="b")
+
+# the output is simply a linear combination of inputs * weights + bias
+p = T.dot(x, w) + b
+
+# error - squared error
+error = squared_error(p,y)
+loss = error.mean() + lamb * L2(w)
+gw, gb = T.grad(loss, [w, b])
+
+
+# training function
+train = theano.function(inputs=[x,y],
+                        outputs=[p, error], 
+                        updates=((w, w - alpha * gw),(b, b - alpha * gb)))
+# prediction function
+predict = theano.function(inputs=[x], outputs=p)
+
+# before training
+print("RMSE before training:", 
+      mean_squared_error(D[1],predict(D[0])))
+
+# training
+for i in range(training_steps):
+    prediction, error = train(D[0], D[1])
+
+# after training
+print("RMSE after training:", 
+      mean_squared_error(D[1],predict(D[0])))
