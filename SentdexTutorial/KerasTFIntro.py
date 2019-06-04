54e686d8cc33b6984f168d7e480219b04ca9db5e
Satoru Hayasaka <hayasaka@utexas.edu>
2019-06-03 23:53:33 -0500

    added the prediction outcome

--- SentdexTutorial/KerasTFIntro.py
+++ SentdexTutorial/KerasTFIntro.py
@@ -56,3 +56,17 @@ model.fit(x_train, y_train, epochs=3)
 val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
 print(val_loss)  # model's loss (error)
 print(val_acc)  # model's accuracy
+
+
+
+##### Saving and loading the model
+model.save('epic_num_reader.model')
+new_model = tf.keras.models.load_model('epic_num_reader.model')
+
+
+####### predictions on the testing data set
+predictions = new_model.predict(x_test)
+
+print(np.argmax(predictions[0]))
+plt.imshow(x_test[0].reshape(shapeOrig),cmap=plt.cm.binary)
+plt.show()
