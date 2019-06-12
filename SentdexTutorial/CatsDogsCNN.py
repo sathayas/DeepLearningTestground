371b6efd8c29162d29eb6b69b381f2364d67d62c
Satoru Hayasaka <hayasaka@utexas.edu>
2019-06-12 00:00:46 -0500

    Now read all data as the training data'

--- SentdexTutorial/CatsDogsCNN.py
+++ SentdexTutorial/CatsDogsCNN.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import cv2
+import random
 from tqdm import tqdm
 
 ### Parameters
@@ -26,3 +27,31 @@ for category in CATEGORIES:  # do dogs and cats
 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
 plt.imshow(new_array, cmap='gray')
 plt.show()
+
+
+
+### Creating the training data
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:  # do dogs and cats
+
+        path = os.path.join(DATADIR,category)  # create path to dogs and cats
+        class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
+
+        for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
+            try:
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
+                training_data.append([new_array, class_num])  # add this to our training_data
+            except Exception as e:  # in the interest in keeping the output clean...
+                pass
+            #except OSError as e:
+            #    print("OSErrroBad img most likely", e, os.path.join(path,img))
+            #except Exception as e:
+            #    print("general exception", e, os.path.join(path,img))
+
+
+create_training_data()
+
+print(len(training_data))
